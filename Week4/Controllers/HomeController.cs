d5f99c29aa132fa6fdfab67b296bcc4d162377fd
Joris Rietveld <jorisrietveld@gmail.com>
2017-05-10 12:30:03 +0200

    Add project files.

---
+++ Week4/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Week4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Senario41()
+        {
+            return View();
+        }
+
+        public IActionResult Senario42( string name, string email )
+        {
+            ViewData["Name"] = name;
+            ViewData["Email"] = email;
+            return View();
+        }
+
+        public IActionResult Senario43()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
